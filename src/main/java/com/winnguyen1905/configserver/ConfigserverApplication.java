38f7514af7a2fcccd20a3a0be8d1cf4a9adc3144
Win Nguyen <233582892+winnguyen1905-aladin@users.noreply.github.com>
2023-06-19 11:30:26 +0700

    server registry

---
+++ src/main/java/com/winnguyen1905/configserver/ConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.winnguyen1905.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winnguyen1905/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winnguyen1905.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
